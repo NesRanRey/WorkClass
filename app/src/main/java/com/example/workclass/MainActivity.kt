afe9504455f9ea15adf1a925139fc97f0c6aa28b
NesRanRey <22151140@aguascalientes.tecnm.mx>
2025-02-10 07:53:07 -0600

    Use of simple layout components

--- app/src/main/java/com/example/workclass/MainActivity.kt
+++ app/src/main/java/com/example/workclass/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -19,8 +22,20 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge()
         setContent {
             WorkClassTheme {
-                Text(   text = "Hello World!")
-                Text(   text = "Welcome to your first application in Kotlin ")
+                Row (){
+                    Column (){
+                        Text(text = "Hello World!")
+                        Text(text = "Welcome to your first application in Kotlin ")
+                    }
+                    Column (){
+                    Text(text = "Hello World!")
+                    Text(text = "Welcome to your first application in Kotlin ")
+                    }
+                    Column (){
+                    Text(text = "Hello World!")
+                    Text(text = "Welcome to your first application in Kotlin ")
+                }
+                }
                 /*
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
@@ -33,7 +48,10 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
+
+
+    }
+
 /*
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {

0dfdb9a63ab9da26edafc2a1f89346190b24c380
NesRanRey <22151140@aguascalientes.tecnm.mx>
2025-02-14 08:16:27 -0600

    Three new classes were added and buttons were added

--- app/src/main/java/com/example/workclass/MainActivity.kt
+++ app/src/main/java/com/example/workclass/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -24,7 +25,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
@@ -32,7 +35,15 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.composable
+import com.example.workclass.ui.screens.HomeScreen
 import com.example.workclass.ui.theme.WorkClassTheme
+import com.example.workclass.ui.screens.MainMenuScreen
+import com.example.workclass.ui.screens.TestScreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,7 +51,8 @@ class MainActivity : ComponentActivity() {
         //enableEdgeToEdge()
         setContent {
             WorkClassTheme {
-                Column() {
+                ComposableMultiScreenApp()
+               /* Column() {
                     Column() {
                       Column (){
                          TextComposable("Nestor")
@@ -58,96 +70,29 @@ class MainActivity : ComponentActivity() {
                             ModifierExample1()
                             ModifierExample3()
                             CustomText()
-                        }
+                            picture()
+                        }*/
                 }
             }
         }
     }
 
-}
 
-@Preview(showBackground = true)
+
+
+    @Composable
+fun ComposableMultiScreenApp(){
+        val navController = rememberNavController()
+        SetupNavGraph(navController = navController)
+}
 @Composable
-fun TextComposable (name:String = "Empty"){
-    Text ("Welcome")
-    Text (name)
+fun SetupNavGraph(navController: NavHostController){
+    NavHost(navController=navController, startDestination= "home_screen"){
+        composable("main_menu"){MainMenuScreen(navController) }
+        composable("home_screen"){HomeScreen(navController) }
+        composable("test_screen"){ TestScreen(navController) }
+    }
+
 }
-    @Preview(showBackground = true)
-    @Composable
-    fun ModifierExample1(){
-        Column (
-            modifier = Modifier
-                .padding(24.dp)
-                .fillMaxWidth()
-                .clickable (onClick={ })
-        ){
-            Text("Hello World")
-        }
-    }
-    fun clickAction (){
-        println("Column Clicked")
-    }
-    @Preview(showBackground = true)
-    @Composable
-    fun ModifierExample2(){
-        Column (
-            modifier = Modifier
-                .padding(24.dp)
-                .fillMaxHeight()
-                .background(Color.Yellow)
-                .border(width = 2.dp, color = Color.White)
-                .width(200.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
-        ){
-            TextComposable("1")
-            TextComposable("2")
-            TextComposable("3")
-            TextComposable("4")
-        }
-
-        }
-    @Preview(showBackground = true)
-    @Composable
-    fun ModifierExample3(){
-        Box (
-            modifier= Modifier
-                .background(Color.Red)
-                .padding(10.dp)
-                .height(300.dp)
-                .width(300.dp)
-        ){
-            Text("1", Modifier.align(Alignment.TopStart))
-            Text("2", Modifier.align(Alignment.TopCenter))
-            Text("3", Modifier.align(Alignment.TopEnd))
-            Text("4", Modifier.align(Alignment.CenterStart))
-            Text("5", Modifier.align(Alignment.Center))
-            Text("6", Modifier.align(Alignment.CenterEnd))
-            Text("7", Modifier.align(Alignment.BottomStart))
-            Text("8", Modifier.align(Alignment.BottomCenter))
-            Text("9", Modifier.align(Alignment.BottomEnd))
 
 
-        }
-    }
-    @Preview(showBackground = true)
-    @Composable
-    fun CustomText() {
-        Column(){
-            Text(
-                stringResource(R.string.sample_text),
-                color = colorResource(R.color.purple_200),
-                fontSize = 20.sp,
-                fontStyle= FontStyle.Italic,
-                fontWeight = FontWeight.ExtraBold
-
-            )
-            val gradientColors = listOf(Color.Cyan, Color.Yellow, Color.DarkGray)
-            Text(
-                stringResource(R.string.sample_text),
-                style= TextStyle(brush = Brush.linearGradient(colors = gradientColors))
-            )
-        }
-    }
-
-    }
